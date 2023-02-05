217e4bb2be3f53dfdac757eeade6b3fb2fdfe2ba
fredboy <fredboy@protonmail.com>
2023-02-05 18:34:52 +0700

    fix publishing native binaries

--- scripts/release.mk
+++ scripts/release.mk
@@ -3,7 +3,7 @@ _build/$(TARGET)/tox4j/libtox4j-c$(DLLEXT): $(PREFIX)/tox4j.stamp
 	touch $@
 
 release: _build/$(TARGET)/tox4j/libtox4j-c$(DLLEXT)
-	rm -rf $(wildcard $(SRCDIR)/tox4j/cpp/src/main/resources/im/tox/tox4j/impl/jni/*/)
-	mkdir -p $(SRCDIR)/tox4j/cpp/src/main/resources/im/tox/tox4j/impl/jni/$(TOX4J_PLATFORM)/
-	cp $< $(SRCDIR)/tox4j/cpp/src/main/resources/im/tox/tox4j/impl/jni/$(TOX4J_PLATFORM)/
-	cd $(SRCDIR)/tox4j/cpp && sbt publishM2
+	rm -rf $(wildcard cpp/src/main/resources/im/tox/tox4j/impl/jni/*/)
+	mkdir -p cpp/src/main/resources/im/tox/tox4j/impl/jni/$(TOX4J_PLATFORM)/
+	cp $< cpp/src/main/resources/im/tox/tox4j/impl/jni/$(TOX4J_PLATFORM)/
+	cd cpp && sbt publishM2
